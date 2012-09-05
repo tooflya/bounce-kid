b9cf9c4e56a8375adca4b9b50750d803cbdb11f3
Igor Mats <igor.mats@yandex.ru>
2012-09-05 23:55:58 +0300

    Add the main class

---
+++ src/com/tooflya/bouncekid/GameActivity.java
@@ -0,0 +1,26 @@
+package com.tooflya.bouncekid;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+
+public class GameActivity extends SimpleBaseGameActivity {
+
+	@Override
+	public EngineOptions onCreateEngineOptions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void onCreateResources() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected Scene onCreateScene() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
