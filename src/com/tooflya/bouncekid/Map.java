a23c48bdcf282e95cb8127306e127f9bdd5fd07b
Rostyslav Pudlo <Rostyslav.Pudlo@Tooflya.com>
2012-09-08 21:07:22 +0300

    Add Map and Block.

---
+++ src/com/tooflya/bouncekid/Map.java
@@ -0,0 +1,19 @@
+package com.tooflya.bouncekid;
+
+import org.anddev.andengine.entity.Entity;
+
+public class Map extends Entity {
+
+	public Map() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.anddev.andengine.entity.sprite.AnimatedSprite#onManagedUpdate (float)
+	 */
+	@Override
+	protected void onManagedUpdate(final float pSecondsElapsed) {
+		super.onManagedUpdate(pSecondsElapsed);
+	}
+}
---
+++ src/com/tooflya/bouncekid/entity/Block.java
@@ -0,0 +1,17 @@
+package com.tooflya.bouncekid.entity;
+
+import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
+
+public class Block extends Entity {
+
+	public Block(TiledTextureRegion pTiledTextureRegion) {
+		super(pTiledTextureRegion);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Block(int x, int y, TiledTextureRegion pTiledTextureRegion) {
+		super(x, y, pTiledTextureRegion);
+		// TODO Auto-generated constructor stub
+	}
+
+}
